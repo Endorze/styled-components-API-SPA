2db093d136ba2ae4cfd84eb71023a25372a2ce92
Endorze <alexyurihallgren@hotmail.com>
2025-05-14 11:04:34 +0200

    feat: Finished setup of basic filestructure for workstructure

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+    colors: {
+        background: "#2d1d2e",
+        primaryColor: "#ffffff",
+        subTitleColor: "ffffff",
+    }
+}
+
+
+export type ThemeType = typeof theme;
